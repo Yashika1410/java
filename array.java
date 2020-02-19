c0e5d8999280d24d8676b15324d0c8cfa5e55ed8
Yashika1410 <61237800+Yashika1410@users.noreply.github.com>
2020-02-19 19:33:11 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,14 @@
+import.java.util.*;
+class array{
+public static void main (String[] a){
+int []a1=new int[4];
+Scanner sc=new Scanner(System.in);
+for(int i=0;i<4;i++)
+{
+int a[i]=sc.nextInt();
+}
+for (int i=0;i<4;i++)
+{
+System.out.println(a[i]);
+}
+}}
